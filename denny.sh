2e52c35f668d051bde3cdc1553ab9404deebb46b
Adaokanga <141701489+Adaokanga@users.noreply.github.com>
2025-05-27 11:58:12 +0100

    Update denny.sh

--- denny.sh
+++ denny.sh
@@ -1,7 +1,5 @@
 termux-setup-storage
-apt-get install python3 && apt-get install git  
-apt-get install pip
-pip install -r req.txt
+apt-get install python3 && apt-get install git && apt-get install pip && pip install -r req.txt
 mkdir -p $HOME/../opt
 mv $HOME/KANGA-HACK/*.py $HOME/../opt/
 mv KANGA $HOME/../usr/bin/KANGA
