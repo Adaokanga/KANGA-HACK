f48743ccbccd021d0c2c001ff4fa658f7edba243
Denny-a_Gvo <141701489+Adaokanga@users.noreply.github.com>
2025-12-20 23:07:24 +0100

    Add files via upload

---
+++ denny.sh
@@ -0,0 +1,9 @@
+termux-setup-storage
+apt-get install python3 && apt-get install git && apt-get install pip && pip install -r req.txt && pkg install unzip
+unzip KANGA-HACK.zip -d KANGA-HACK
+mkdir -p $HOME/../opt
+mv $HOME/KANGA-HACK/KANGA-HACK/KANGA $HOME/../usr/bin/KANGA
+chmod +x $HOME/../usr/bin/KANGA
+mv $HOME/KANGA-HACK/KANGA-HACK $HOME/../opt/
+cd
+rm -rf KANGA-HACK
